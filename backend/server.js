e491e23346a0d736eabd27135513913aae04d839
Akash Ramireddy <akash1194r@gmail.com>
2025-09-21 14:40:59 +0530

    Vercel deployment setup.

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,6 @@ app.use('/api/credit', creditRouter)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+// app.listen(PORT, ()=>{
+//     console.log(`Server is running on port ${PORT}`);
+// })
